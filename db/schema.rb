b3b4a4fc54a2e4444cda5b9c674e7ec7b09b669b
littleyang <littleyangqh@gmail.com>
2012-09-24 21:07:05 +0800

    tetx

--- db/schema.rb
+++ db/schema.rb
@@ -65,6 +65,13 @@ ActiveRecord::Schema.define(:version => 20120924090343) do
     t.boolean  "status",     :default => false
   end
 
+  create_table "jokes", :force => true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "taggings", :force => true do |t|
     t.integer  "tag_id"
     t.integer  "taggable_id"
