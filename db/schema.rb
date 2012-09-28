7157bb96bf7fb127254dba8d051a2050f6c6f6e2
littleyang <littleyangqh@gmail.com>
2012-09-28 19:30:20 +0800

    add user migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120928070918) do
+ActiveRecord::Schema.define(:version => 20120928111806) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -67,6 +67,13 @@ ActiveRecord::Schema.define(:version => 20120928070918) do
     t.boolean  "status",     :default => false
   end
 
+  create_table "jokes", :force => true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "links", :force => true do |t|
     t.string   "title"
     t.string   "url"
@@ -99,7 +106,6 @@ ActiveRecord::Schema.define(:version => 20120928070918) do
     t.datetime "created_at",                              :null => false
     t.datetime "updated_at",                              :null => false
     t.string   "permission",             :default => "2"
-    t.string   "email",                  :default => "",  :null => false
     t.string   "encrypted_password",     :default => "",  :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -109,9 +115,9 @@ ActiveRecord::Schema.define(:version => 20120928070918) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "email",                                   :null => false
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
